2a93ba4f36e2f3fe09b96c6dcaa49c5fe88f81d4
UmeshPatel111 <umeshr.technocomet@gmail.com>
2025-08-01 18:55:43 +0530

    forgot passwored bug solve

--- src/components/VerificationCode.tsx
+++ src/components/VerificationCode.tsx
@@ -3,35 +3,40 @@ import React from 'react'
 import { VerifyOtp } from './form/LoginForms'
 import Image from 'next/image'
 import Logo from "../assets/images/Maia Logo.png";
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function VerificationCode() {
     return (
         <>
-        <div className='d-flex align-items-center justify-content-center vh-100 verfication-code-main'>
-        <div className="verificationcode-main d-block align-items-center justify-content-center">
-            <div className=''>
-                <div className='d-flex justify-content-center align-item-center'>
-                    <Image
-                        src={Logo}
-                        alt="MAIA Care Logo"
-                        width={145}
-                        height={45}
-                        className="mb-2"
-                    />
-                </div>
+            <Container fluid className="d-flex align-items-center justify-content-center vh-100">
+                <Row className='w-100'>
+                    <Col  md={7} lg={8} xl={5} className="mx-auto">
+                        <div className='verificationcode-main d-block align-items-center justify-content-center'>
+                            <div className=''>
+                                <div className='d-flex justify-content-center align-item-center'>
+                                    <Image
+                                        src={Logo}
+                                        alt="MAIA Care Logo"
+                                        width={145}
+                                        height={45}
+                                        className="mb-2"
+                                    />
+                                </div>
 
-                <h2 className="login-title text-center mt-3">Forgot Password</h2>
-                <p className="login-subtitle text-center">
-                An email with a verification code was just sent to bm@gmail.com
-                </p>
+                                <h2 className="login-title text-center mt-3">Forgot Password</h2>
+                                <p className="login-subtitle text-center">
+                                    An email with a verification code was just sent to bm@gmail.com
+                                </p>
 
 
-            </div>
+                            </div>
 
-            <VerifyOtp />
+                            <VerifyOtp />
 
-        </div>
-        </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
