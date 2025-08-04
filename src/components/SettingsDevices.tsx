7202d69d54eb30e0e662a15377adb2df1aaef74d
pankajdev-dot <pankaj.technocomet@gmail.com>
2025-08-04 09:31:34 +0530

    password-setting-resposive

--- src/components/SettingsDevices.tsx
+++ src/components/SettingsDevices.tsx
@@ -3,15 +3,17 @@ import Button from './ui/Button'
 import Image from 'next/image'
 import { FiLogOut } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import deviceWindowsImg from "@/assets/images/device-windows.png";
+import deviceMobileImg from "@/assets/images/device-mobile.png";
 
 const SettingsDevices = () => {
     return (
         <>
-            <p className="settings-accordion-subtitle">Where you're signed in</p>
+            <p className="settings-accordion-subtitle my-4">Where you're signed in</p>
             <div className="d-flex flex-column gap-4 ">
                 <div className="your-device-box d-flex flex-column flex-lg-row justify-content-between gap-3 align-items-start align-items-lg-center">
                     <div className="d-flex justify-content-center align-items-center gap-3 ">
-                        <Image src="/images/device-windows.png" width={70} height={70} alt="device-window" />
+                        <Image src={deviceWindowsImg} width={70} height={70} alt="device-window" />
                         <div>
                             <h6 className="your-device-box-title mb-1">Windows Computer</h6>
                             <p className="your-device-box-subtitle mb-0 ">Google Chrome | Mumbai Logged In</p>
@@ -31,7 +33,7 @@ const SettingsDevices = () => {
 
                 <div className="your-device-box d-flex flex-column flex-lg-row justify-content-between gap-3 align-items-start align-items-lg-center">
                     <div className="d-flex justify-content-center align-items-center gap-3 ">
-                        <Image src="/images/device-mobile.png" width={70} height={70} alt="device-window" />
+                        <Image src={deviceMobileImg} width={70} height={70} alt="device-window" />
                         <div>
                             <h6 className="your-device-box-title mb-1">Redmi Note 13 Pro</h6>
                             <p className="your-device-box-subtitle mb-0">Google Chrome | Mumbai Logged In</p>
