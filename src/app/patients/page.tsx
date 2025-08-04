36fa81e35a7f34895e9e968979ed87f349fd3089
pankajdev-dot <pankaj.technocomet@gmail.com>
2025-08-04 18:33:41 +0530

    add -Physical Assessment (Modal),Physical Assessment with accordion (Modal)

--- src/app/patients/page.tsx
+++ src/app/patients/page.tsx
@@ -2,17 +2,30 @@
 
 import { AppDispatch } from "@/utils/redux/store";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { setHeaderData } from "@/utils/redux/slices/headerSlice";
+import Modal from "@/components/ui/Modal";
+import Button from "@/components/ui/Button";
+import { InputFieldGroup } from "@/components/ui/InputField";
+import { Col, Row } from "react-bootstrap";
+import InputSelect from "@/components/ui/InputSelect";
+import PhisicalAssessment from "@/components/PhisicalAssessment";
+import FertilityAssessment from "@/components/FertilityAssessment";
+import "@/style/fertilityassessment.css";
+import "@/style/settingsPassword.css";
 
 
 function Page() {
-    const dispatch: AppDispatch = useDispatch();
+  const dispatch: AppDispatch = useDispatch();
   useEffect(() => {
     dispatch(setHeaderData({ title: "Patients", subtitle: "Patients List" }));
   }, []);
+
   return <div>
-    <h1>Patients</h1>
+    {/* <h1>Patients</h1> */}
+    <PhisicalAssessment />
+    <FertilityAssessment />
+
   </div>;
 }
 
