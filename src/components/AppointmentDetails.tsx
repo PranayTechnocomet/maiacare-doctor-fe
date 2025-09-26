3ede2a28f0e2054c564512646d0ed81efa78e0d8
pankajdev-dot <pankaj.technocomet@gmail.com>
2025-09-26 17:22:37 +0530

    Blood Group field disabled and accordion arrow fix

--- src/components/AppointmentDetails.tsx
+++ src/components/AppointmentDetails.tsx
@@ -386,7 +386,11 @@ function AppointmentDetails() {
 
                         </> : <>
                             {PaymentFormShow == true ?
-                                <AppointmentPaymentDetails setPaymentFormShow={setPaymentFormShow} setPaymentFormData={setPaymentFormData} PaymentFormData={PaymentFormData} />
+                                <AppointmentPaymentDetails
+                                    setPaymentFormShow={setPaymentFormShow}
+                                    setPaymentFormData={setPaymentFormData}
+                                    PaymentFormData={PaymentFormData}
+                                />
                                 :
                                 <div className='text-center mt-4'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80" fill="none">
