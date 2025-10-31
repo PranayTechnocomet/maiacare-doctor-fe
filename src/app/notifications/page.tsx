75fe96a68156b1f2cfa4343b53ec434872766835
UmeshPatel111 <umeshr.technocomet@gmail.com>
2025-10-31 19:11:55 +0530

    comon  componet add

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,10 @@
+import NotificationScreen from '@/components/NotificationScreen'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <NotificationScreen />
+    </div>
+  )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,85 @@
+'use client'
+import { useState } from 'react';
+import CustomTabs from './ui/CustomTabs';
+import ContentContainer from './ui/ContentContainer';
+import DoctorProfile from './ui/custom/DoctorProfile';
+import ProfileBasicDetailsTabs from './Profile-Basic-Details';
+import ManageLeave from './ManageLeave';
+
+const ProfileTabes = () => {
+    const [activeTab, setActiveTab] = useState<string>("basic");
+
+    const tabOptions = [
+        {
+            key: "basic",
+            label: "Basic Details",
+            content: (
+                <>
+                    <ProfileBasicDetailsTabs />
+                    
+                </>
+            ),
+        },
+        {
+            key: "leaves",
+            label: "Manage Leaves",
+            content: (
+                <>
+                    <ManageLeave />
+                    
+                </>
+            ),
+        },
+        {
+            key: "reviews",
+            label: "Reviews",
+            content: (
+                <>
+                    <ContentContainer className="mt-5">
+                        <h1>Reviews Content</h1>
+                    </ContentContainer>
+                </>
+            ),
+        },
+
+    ];
+
+
+    return (
+        <>
+            <DoctorProfile />
+            <div className='mt-4'>
+
+                <CustomTabs
+                    activeKey={activeTab}
+                    setActiveKey={setActiveTab}
+                    tabOptions={tabOptions}
+                />
+
+                {/* {activeTab === 'basic' && (
+          <div>
+
+          </div>
+        )}
+
+        {activeTab === 'leaves' && (
+          <div>
+            
+          </div>
+        )}
+
+        {activeTab === 'Reviews' && (
+          <div>Reviews Content</div>
+        )} */}
+
+            </div>
+        </>
+
+
+
+
+
+    );
+};
+
+export default ProfileTabes;
